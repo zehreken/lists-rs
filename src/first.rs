cef6cc193b0f550178ed1995bf23a1c9d182b933
zehreken <zehreken@gmail.com>
2020-08-12 00:05:35 +0200

    Add Drop impl for List

--- src/first.rs
+++ src/first.rs
@@ -29,6 +29,16 @@ impl List {
     }
 }
 
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
+}
+
 enum Link {
     Empty,
     More(Box<Node>),
